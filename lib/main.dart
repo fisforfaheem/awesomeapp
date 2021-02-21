f49cb6764124b53926b8ca9c0c01a28486c5d8e5
Faheem <fisforfaheem@gmail.com>
2021-02-21 17:44:23 +0500

    chapter 1 completed

--- lib/main.dart
+++ lib/main.dart
@@ -16,30 +16,40 @@ class HomePage extends StatelessWidget {
           title: Text("Awesome App"),
           backgroundColor: Colors.green[600],
         ),
-        body: Center(
-          child: Container(
-            padding: const EdgeInsets.all(20.0),
-            alignment: Alignment.center,
-            width: 150,
-            height: 150,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(20),
-              boxShadow: [
-                BoxShadow(
-                    color: Colors.grey[400],
-                    blurRadius: 5,
-                    offset: Offset(2.0, 5.0))
-              ],
-              color: Colors.teal,
-              gradient: LinearGradient(colors: [Colors.yellow, Colors.pink]),
-            ),
-            child: Text(
-              "hi buddy",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
-                fontSize: 20.0,
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Align(
+            alignment: Alignment.bottomRight,
+            child: Container(
+              width: 200,
+              height: 400,
+              color: Colors.black,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.all(8.0),
+                      width: 100,
+                      height: 100,
+                      color: Colors.green,
+                    ),
+                    Container(
+                      padding: const EdgeInsets.all(8.0),
+                      width: 100,
+                      height: 100,
+                      color: Colors.yellow,
+                    ),
+                    Container(
+                      padding: const EdgeInsets.all(8.0),
+                      width: 100,
+                      height: 100,
+                      color: Colors.orange,
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
